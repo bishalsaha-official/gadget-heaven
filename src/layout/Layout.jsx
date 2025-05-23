13b246cdd6d0ca51b0852f9e2ecd7a5152b3d271
bishalsaha-official <cse.bishalsaha@gmail.com>
2025-05-23 20:10:55 +0600

    Implement Navbar Components

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+const Layout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            
+        </div>
+    );
+};
+
+export default Layout;
